53a2cefdaef55eac9fd88836132bf8d70e88b75d
dunduru8 <dunduru1@gmail.com>
2018-12-25 04:30:12 +0200

    Осталась последняя проблема - в корзине не затирается старая инфа, а просто рядом пишется новая.

--- main_6_catalog.js
+++ main_6_catalog.js
@@ -5,47 +5,57 @@ var product = [
     Price: 35.70,
     color: "black",
     availability: "в наличии",
+    Quant: 1,
 },
 {
     Name: "Coat",
     Price: 150.00,
     color: "black",
-    availability: "в наличии"
+    availability: "в наличии",
+    Quant: 1,
 },    
 
 {
     Name: "Jacket",
     Price: 55.00,
     color: "navy blue",
-    availability: "в наличии"
+    availability: "в наличии",
+    Quant: 1,
 },
 {
     Name: "T-shirt",
     Price: 25.00,
     color: "gray",
     availability: "в наличии",
+    Quant: 1,
 },
 {
     Name: "Sweater",
     Price: 55.00,
     color: "gray",
     availability: "в наличии",
+    Quant: 1,
 },
 {
     Name: "Vest",
     Price: 50.00,
     color: "blue",
     availability: "в наличии",
+    Quant: 1,
 }
-]
+];
+
     
- var cart = [];  //переменная для массива корзины
+ var cart = [];
 
 // Создание каталога
 
 function catalogCreation(catalog){
     var $catalogContent = document.getElementById("catalog");
- for(var i=0; i < catalog.length; i++){ 
+ 
+ var $item = document.getElementById("item");
+    
+for(var i=0; i < catalog.length; i++){ 
      var $catalogItem = document.createElement("div");
      $catalogItem.classList.toggle("item");
      $catalogItem.innerHTML = catalog[i].Name;
@@ -66,12 +76,11 @@ function catalogCreation(catalog){
      $itemAvail.textContent = "Есть " + catalog[i].availability; 
      $catalogItem.appendChild($itemAvail);
      
-     var $buttonBuy = document.createElement("div");
+     var $buttonBuy = document.createElement("button");
      $buttonBuy.className = "buttonBuy";
      $buttonBuy.textContent = "Купить"; 
      $buttonBuy.id = catalog[i].Name;
      $catalogItem.appendChild($buttonBuy);
-     
      
     }
     return $catalogContent;
@@ -79,22 +88,54 @@ function catalogCreation(catalog){
 };
 catalogCreation(product);
 
-for( var i = 0; i < product.length; i++){
-   var $idCreator = product[i].Name; 
-   var $buttonBuy = document.getElementById($idCreator);
-   var objInCart = {};
-   $buttonBuy.addEventListener("click", function(){
-     [].forEach.call($buttonBuy, function(el){
-        $buttonBuy.click = function(el){
-           for( var i = 0; i < product.length; i++){
-               var id = product[i].Name;
-               var idTempor = this.id;
-                   if ( id == idTempor){
-                    objInCart = product[i];
-                    cart.push(objInCart);
-                    }
-           }
+document.body.addEventListener("click", function(Event){
+    if (event.target.nodeName == "BUTTON"){
+        var objInCart = {};
+        for( var i = 0; i < product.length; i++){
+        var id = product[i].Name;
+        var idTempor = event.target.id;
+          if ( id == idTempor){
+            objInCart = product[i];
+            cart.push(objInCart);      
+            console.log("click", event.target.id, cart);
+          }
         }
-      })
-    })
-};  
+    };
+    cartDivCreation(); 
+});
+
+function cartSumm(){                                  
+  var cartPrice = 0;
+  for (var i = 0; i< cart.length; i++){
+     var temporItemPrice = cart[i]["Price"] * cart[i]["Quant"];
+     cartPrice += temporItemPrice;
+  }
+  return cartPrice;    
+}  //Подсчет общей суммы корзины
+
+function cartItem(){
+  var cartItem = 0;
+  for (var i = 0; i< cart.length; i++){
+     var temporItemNum = cart[i]["Quant"];
+     cartItem += temporItemNum;
+  }
+  return cartItem;        
+}   //Подсчет количества товаров
+
+function cartDivCreation(){
+  if (cart == 0){
+     var $cartContent = document.getElementById("cart");
+     $cartContent.innerHTML = "Корзина пуста";   
+   }else{
+       $cartContent = ["В корзине", cartItem(), "товаров", "на сумму", cartSumm(), "eur"]    
+       for(var i=0; i < $cartContent.length; i++){ 
+          var $cartDiv = document.createElement("div");  
+          $cartDiv.innerHTML = $cartContent[i];
+          $cartDiv.className = "cartDiv";
+          document.getElementById("cart").appendChild($cartDiv);     
+        }       
+    }
+     return $cartContent;
+}
+
+cartDivCreation();
